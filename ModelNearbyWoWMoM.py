7b3bb25115c03598ba1486c8606240347d9019db
Clément Bertier <clement.bertier@lip6.fr>
2019-03-08 10:51:06 +0100

    Add the modulations and propagations library

---
+++ ModelNearbyWoWMoM.py
@@ -0,0 +1,165 @@
+#!/usr/bin/python
+"""
+Library to estimate the throughput according to distance.
+"""
+import numpy as np
+from scipy.constants import speed_of_light
+import matplotlib.pyplot as plt
+from lmfit import Model,Parameters
+import matplotlib.font_manager as fm
+import json
+import matplotlib
+import glob
+#prop = fm.FontProperties(fname='/usr/share/fonts/truetype/liberation/LiberationSerif-Regular.ttf',size=42)
+prop = fm.FontProperties(fname='/usr/share/fonts/truetype/liberation/LiberationSerif-Regular.ttf',size=12)
+
+"""
+CONSTANTS
+"""
+min_rssi=87
+freq=5180*10**6
+wavelength=speed_of_light/freq
+threshold_rssi=np.array([0,-55,-57,-58,-59,-63,-67,-70,-72,-75,-min_rssi])
+data_rate=[866,780,650,585,520,390,260,195,130,65]
+
+"""
+BASIC FUNCTIONS
+"""
+def linear(rssi,a,b):
+    return a*rssi+b
+
+def Wifi5_goodput_bottleneck(rssi):
+    return 24.267885
+
+"""
+PROPAGATION MODELS
+"""
+
+def freespace_loss(distance,Tx=10):
+    loss=20*np.log10(4*np.pi*distance/wavelength)
+    return loss if loss > 0 else 0
+
+def logDistance_loss(distance,alpha=3):
+    d0=1
+    if distance > d0:
+        loss=freespace_loss(d0)+10*alpha*np.log10(distance/d0)
+        return loss if loss > 0 else 0
+    else:
+        return freespace_loss(distance)
+
+def twoRay_loss(distance, epsilon_r=1.00673130,height=1.39):
+    #Tx=10.2
+    #Tx=9.19
+    height_sender=height
+    height_receiver=height
+    d_reflection=np.sqrt( (distance**2) + (height_sender + height_receiver)**2)
+    d_LoS= np.sqrt( (distance**2) + (height_sender - height_receiver)**2)
+    phi=2*np.pi* ((d_LoS - d_reflection) / wavelength)
+    sin_theta= (height_sender + height_receiver) / d_reflection
+    cos_theta = (distance) / d_reflection
+    gamma= (sin_theta - np.sqrt(epsilon_r - cos_theta**2)) / (sin_theta + np.sqrt(epsilon_r - cos_theta **2))
+    LossTwoRay=10*np.log10((4 * np.pi * (distance / wavelength) * 1 / (np.sqrt(((1 + gamma * np.cos(phi))**2 + gamma**2 * np.sin(phi)**2))))**2)
+    return LossTwoRay if LossTwoRay > 0 else 0
+
+"""
+MODULATION SCHEMES
+"""
+
+def Wifi5_empirical_goodput(rssi):
+    rssi=np.absolute(rssi)
+    """
+    ]0,64] = 24.267885
+    ]64,71] = -0.359363*x+42.356715
+    ]71,82] = -0.791134*x+66.438033
+    ]82,88] = -0.236949*x+21.128388
+    """
+    limits=[0,64,71,82,min_rssi]
+    funcs=[Wifi5_goodput_bottleneck, linear, linear, linear]
+    args=[-1,[-0.359363,42.356715],[-0.791134,66.438033],[-0.236949,21.128388]]
+    if rssi > limits[-1]:
+        return 0
+    
+    for j,lower in enumerate(limits):
+        if lower < rssi <= limits[j+1]:
+            if hasattr(args[j], "__len__"):
+                return funcs[j](rssi,*args[j])*8
+            else:
+                return funcs[j](rssi)*8
+    return "error"
+
+def Wifi5_stepwise_max(rssi):
+    threshold_rssi=np.array([0,-55,-57,-58,-59,-63,-67,-70,-72,-75,-min_rssi])
+    data_rate=[866,780,650,585,520,390,260,195,130,65]
+    if rssi >= 0:
+        return data_rate[0]
+    elif rssi < threshold_rssi[-1]:
+        return 0
+    else:
+        return data_rate[np.where(threshold_rssi<rssi)[0][0]-1]
+
+def Wifi5_stepwise_linear_adjusted(rssi):
+    threshold_rssi=np.array([0,-55,-57,-58,-59,-63,-67,-70,-72,-75,-min_rssi])
+    data_rate=[866,780,650,585,520,390,260,195,130,65]
+    if rssi >= 0:
+        return data_rate[0]
+    elif rssi < threshold_rssi[-1]:
+        return 0
+    elif np.where(threshold_rssi<=rssi)[0][0]-1 == len(data_rate)-1:
+        return rssi*4.33 + 390
+    else:
+        return data_rate[np.where(threshold_rssi<rssi)[0][0]-1]
+
+def Wifi5_stepwise_fit(rssi):
+    threshold_rssi=np.array([0,-55,-57,-58,-59,-63,-67,-70,-72,-75,-min_rssi])
+    data_rate=np.array([22.894102,24.934630,24.119049,24.727494,23.782886,20.275357,17.555846,14.414938,8.225040,3.574197])*8
+    if rssi >= 0:
+        return data_rate[0]
+    elif rssi <= threshold_rssi[-1]:
+        return 0
+    else:
+        return data_rate[np.where(threshold_rssi<rssi)[0][0]-1]
+
+"""
+HELPER FUNCTIONS
+"""
+
+def RSSI_TO_BPS(rssi,modulation_scheme=Wifi5_empirical_goodput):
+    if isinstance(rssi, (list, tuple, np.ndarray)):
+        res=[]
+        for r in rssi:
+            res.append(modulation_scheme(r))
+        return res
+    elif isinstance(rssi, (float, int)):
+        return modulation_scheme(rssi)
+    else:
+        raise ValueError("Data type not understood by model.")
+    
+def DISTANCE_TO_RSSI(distance,Tx=9.19,pathloss=freespace_loss):
+    if isinstance(distance,(list,tuple,np.ndarray)):
+        res=[]
+        for d in distance:
+            res.append(Tx-pathloss(d))
+        return res
+    elif isinstance(distance,(float,int)):
+        return Tx-pathloss(distance)
+    else:
+        raise ValueError("Data type not understood by model")
+
+propagation_models=[freespace_loss,logDistance_loss,twoRay_loss]
+modulation_schemes=[Wifi5_empirical_goodput,Wifi5_stepwise_max,Wifi5_stepwise_linear_adjusted,Wifi5_stepwise_fit]
+
+if __name__ == "__main__":
+    f, axarr = plt.subplots(3, 1)
+    distances=np.linspace(0.001,400,1000)
+    
+    for propag in propagation_models:
+        axarr[0].plot(distances,DISTANCE_TO_RSSI(distances, pathloss=propag),label=propag.func_name)
+        print distances[np.where(np.array(DISTANCE_TO_RSSI(distances, pathloss=propag)) < -min_rssi)[0][0]]
+        axarr[2].plot(distances,RSSI_TO_BPS(DISTANCE_TO_RSSI(distances,pathloss=propag)),label="%s goodput" % propag.func_name)
+    axarr[0].legend() 
+    rssi=np.linspace(50,-150,1000)
+    for modulation in modulation_schemes:
+        axarr[1].plot(rssi,RSSI_TO_BPS(rssi, modulation_scheme=modulation),label=str(modulation.func_name))
+    axarr[1].legend()
+    plt.show()
+    
---
+++ calculate_goodput_contact.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+"""
+Calculate the contact data-exchange through integral linear interpolation.
+"""
+import networkx as nx
+import numpy as np
+from scipy.integrate import quad
+import ModelNearbyWoWMoM
+import json
+import sys
+import pickle
+
+
+dataset=sys.argv[1]
+propagation_name=sys.argv[2]
+modulation_name=sys.argv[3]
+propagation=False
+modulation=False
+for p in ModelNearbyWoWMoM.propagation_models:
+    if p.func_name == propagation_name:
+        propagation=p
+        break
+if not propagation:
+    print "error, propagation not found"
+    exit(1)
+
+for m in ModelNearbyWoWMoM.modulation_schemes:
+    if m.func_name == modulation_name:
+        modulation=m
+        break
+if not modulation:
+    print "error, modulation not found"
+    exit(1)
+if dataset=="luxembourg":
+    import Luxembourg as ds
+    test_range=[29000,30000]
+elif dataset=="stockholm":
+    import Stockholm as ds
+    test_range=[ds.start_time,ds.end_time-1]
+else:
+    print "Unknown dataset %s" %  dataset
+    exit(1)
+    
+directory="../../data/pickle_graphs/"
+output_dir="../../data/contacts_capacity/"
+#test_range=[14400,79200] # toute la journee
+#test_range=[30000,31200] # peak
+#test_range=[39600,54000] # mid-day
+#test_range=[5000,15000] # night
+
+
+
+
+xa=ds.time_granularity
+xb=ds.time_granularity*2
+time_granularity=ds.time_granularity
+def linear(x, a, b):
+    return a*x + b
+def integrate(ya,yb):
+    a=(yb-ya)/(ds.time_granularity)
+    b=ya-a*xa
+    data_contact,error_integration = quad(linear, xa, xb, args=(a,b))
+    return data_contact
+
+    
+current_contacts={}
+current_contacts_start={}
+terminated_contacts={}
+
+for time in np.arange(test_range[0],test_range[1],ds.time_granularity):
+    current_graph={}
+    for edge,distance in pickle.load(open(directory + "%s_%s.pkl" % (dataset,time), 'rb') ).items():
+        #value_goodput=RSSI_TO_BPS(freespace_Rx(distance,10))
+        rssi=ModelNearbyWoWMoM.DISTANCE_TO_RSSI(distance,pathloss=propagation)
+        value_goodput=ModelNearbyWoWMoM.RSSI_TO_BPS(rssi,modulation_scheme=modulation)
+        if value_goodput > 0:
+            current_graph[edge]=value_goodput
+            
+    current_edges=set(current_graph.keys())
+    if time != test_range[0]:
+        for edge in current_edges - old_edges:
+            #this is a new contact
+            current_distance=current_graph[edge]
+            yb=current_graph[edge]
+            ya=0
+            current_contacts[edge]=integrate(ya,yb)
+            current_contacts_start[edge]=time-ds.time_granularity
+        for edge in current_edges & old_edges:
+            #this is a previously established contact
+            current_distance=current_graph[edge]
+            previous_distance=old_graph[edge]
+            yb=current_graph[edge]
+            ya=old_graph[edge]
+            current_contacts[edge]+=integrate(ya,yb)
+        for edge in old_edges - current_edges:
+            #this is the end of the contact
+            ya=old_graph[edge]
+            yb=0
+            previous_distance=old_graph[edge]
+            final_edge_key="contact:%s;time:%s-%s" % (edge,current_contacts_start[edge],time)
+            terminated_contacts[final_edge_key]=current_contacts[edge] + integrate(ya,yb)
+            del current_contacts[edge]
+            del current_contacts_start[edge]
+    else:
+        for edge in current_edges:
+            #this is a new contact
+            current_distance=current_graph[edge]
+            rssi=ModelNearbyWoWMoM.DISTANCE_TO_RSSI(distance,pathloss=propagation)
+            yb=ModelNearbyWoWMoM.RSSI_TO_BPS(rssi,modulation_scheme=modulation)
+            ya=0
+            current_contacts[edge]=integrate(ya,yb)
+            current_contacts_start[edge]=time
+    old_edges=current_edges
+    old_graph=current_graph
+    print time, len(current_contacts),len(terminated_contacts)
+
+
+
+with open(output_dir+'contacts_data_nearby_%s_%s_%s_%s_%s.json' % (test_range[0],test_range[1],dataset,propagation_name,modulation_name), 'w') as fp:
+    json.dump(terminated_contacts, fp)
+    #print "writing disabled for test. Uncomment line in code to enable writing output."
