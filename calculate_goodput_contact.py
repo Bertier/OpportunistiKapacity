83efe54f520d28709f273a680f743e6260c662ca
Clément Bertier <clement.bertier@lip6.fr>
2019-04-01 14:31:29 +0200

    Fix multiple naming issues

--- calculate_goodput_contact.py
+++ calculate_goodput_contact.py
@@ -69,6 +69,7 @@ current_contacts_start={}
 terminated_contacts={}
 rssi_func=np.vectorize(ModelNearbyWoWMoM.DISTANCE_TO_RSSI)
 bps_func=np.vectorize(ModelNearbyWoWMoM.RSSI_TO_BPS)
+old_edges=set()
 
 #for time in np.arange(test_range[0],test_range[1],ds.time_granularity):
 for time,id_nodes,posx_nodes,posy_nodes in DatasetReader(dataset):
@@ -76,52 +77,54 @@ for time,id_nodes,posx_nodes,posy_nodes in DatasetReader(dataset):
     position_nodes=np.array((posx_nodes,posy_nodes)).astype(float).T
     #Find which nodes are able to exchange data
     distance_between_nodes = spatial.distance.cdist(position_nodes,position_nodes)
-    throughput_between_nodes=bps_func(rssi_func(distance_between_nodes,pathloss=propagation),modulation_scheme=modulation)
-    throughput_between_nodes *= np.tri(*throughput_between_nodes.shape)
-    np.fill_diagonal(throughput_between_nodes,0)
+    throughput_between_nodes= bps_func(rssi_func(distance_between_nodes,pathloss=propagation),\
+                                      modulation_scheme=modulation)
+    throughput_between_nodes *= np.tri(throughput_between_nodes.shape[0],throughput_between_nodes.shape[1],-1)
+    #np.fill_diagonal(throughput_between_nodes,0)
     current_edges_indexes=np.where(throughput_between_nodes > 0)
+    instant_goodput=[]
+    if len(current_edges_indexes[0]):
+        for k in range(current_edges_indexes[0]):
+            node_1=id_nodes[current_edges_indexes[0][k]]
+            node_2=id_nodes[current_edges_indexes[1][k]]
+            edge_key="-".join(sorted([node_1,node_2]))
+            instant_goodput[edge_key]=throughput_between_nodes[current_edges_indexes[0][k],\
+                                                          current_edges_indexes[1][k]]
+    current_edges=set(instant_goodput.keys()) if len(instant_goodput) else set()
+    """
+    There are 3 possbilities.
+    1) The contact is new at this instant, we add it to our dictionnary of contacts
+    2) The contact existed, we simply increment the contact capacity since it lasted longer
+    3) The contact does not exist anymore, we remove it from the current contacts and count it as terminated
+    """
+    #If it is in the current contact, and did not exist is the previous contacts, it is new.
+    for edge in current_edges - old_edges:
+        current_distance=current_graph[edge]
+        yb=current_graph[edge]
+        ya=0
+        current_contacts[edge]=integrate(ya,yb)
+        current_contacts_start[edge]=time-ds.time_granularity
+
+    #If it existed both in previous and current contacts, simply update the capacity.
+    for edge in current_edges & old_edges:
+        #this is a previously established contact
+        current_distance=current_graph[edge]
+        previous_distance=old_graph[edge]
+        yb=current_graph[edge]
+        ya=old_graph[edge]
+        current_contacts[edge]+=integrate(ya,yb)
+
+    #If there are contact that previously existed and are not found in the current one, consider it terminated.
+    for edge in old_edges - current_edges:
+        #this is the end of the contact
+        ya=old_graph[edge]
+        yb=0
+        previous_distance=old_graph[edge]
+        final_edge_key="contact:%s;time:%s-%s" % (edge,current_contacts_start[edge],time)
+        terminated_contacts[final_edge_key]=current_contacts[edge] + integrate(ya,yb)
+        del current_contacts[edge]
+        del current_contacts_start[edge]
     
-    for edge,distance in pickle.load(open(directory + "%s_%s.pkl" % (dataset,time), 'rb') ).items():
-        #value_goodput=RSSI_TO_BPS(freespace_Rx(distance,10))
-        rssi=ModelNearbyWoWMoM.DISTANCE_TO_RSSI(distance,pathloss=propagation)
-        value_goodput=ModelNearbyWoWMoM.RSSI_TO_BPS(rssi,modulation_scheme=modulation)
-        if value_goodput > 0:
-            current_graph[edge]=value_goodput
-            
-    current_edges=set(current_graph.keys())
-    if time != test_range[0]:
-        for edge in current_edges - old_edges:
-            #this is a new contact
-            current_distance=current_graph[edge]
-            yb=current_graph[edge]
-            ya=0
-            current_contacts[edge]=integrate(ya,yb)
-            current_contacts_start[edge]=time-ds.time_granularity
-        for edge in current_edges & old_edges:
-            #this is a previously established contact
-            current_distance=current_graph[edge]
-            previous_distance=old_graph[edge]
-            yb=current_graph[edge]
-            ya=old_graph[edge]
-            current_contacts[edge]+=integrate(ya,yb)
-        for edge in old_edges - current_edges:
-            #this is the end of the contact
-            ya=old_graph[edge]
-            yb=0
-            previous_distance=old_graph[edge]
-            final_edge_key="contact:%s;time:%s-%s" % (edge,current_contacts_start[edge],time)
-            terminated_contacts[final_edge_key]=current_contacts[edge] + integrate(ya,yb)
-            del current_contacts[edge]
-            del current_contacts_start[edge]
-    else:
-        for edge in current_edges:
-            #this is a new contact
-            current_distance=current_graph[edge]
-            rssi=ModelNearbyWoWMoM.DISTANCE_TO_RSSI(distance,pathloss=propagation)
-            yb=ModelNearbyWoWMoM.RSSI_TO_BPS(rssi,modulation_scheme=modulation)
-            ya=0
-            current_contacts[edge]=integrate(ya,yb)
-            current_contacts_start[edge]=time
     old_edges=current_edges
     old_graph=current_graph
     print time, len(current_contacts),len(terminated_contacts)
