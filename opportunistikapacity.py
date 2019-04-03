3355e452540c4c4301523a304bf213c9ff35d8ed
Clément Bertier <clement.bertier@lip6.fr>
2019-04-03 15:09:58 +0200

    Add new class for the calculation of contact traces.

--- opportunistikapacity.py
+++ opportunistikapacity.py
@@ -7,11 +7,12 @@ import os
 from decimal import Decimal
 import numpy as np
 from scipy.integrate import quad
-import json
-from DatasetReader import DatasetReader
 from scipy import spatial
-import networkx as nx
+from datetime import datetime
+import json
+from DatasetReader import DatasetReader,ContactReader
 import ModelNearbyWoWMoM
+
 
 class GeographicTrace(object):
     #TODO: get granularity from dataset
@@ -64,15 +65,15 @@ class GeographicTrace(object):
             instant_edges=set(instant_goodput.keys()) if len(instant_goodput) else set()
             """
             There are 3 possbilities.
-            1) The contact is new at this instant, we add it to our dictionnary of contacts
+            1) The contact is new at this instant, we add it to our dictionnary of active contacts
             2) The contact existed, we simply increment the contact capacity since it lasted longer
             3) The contact does not exist anymore, we remove it from the current contacts and count it as terminated
             """
             #If it is in the current contact, and did not exist is the previous contacts, it is new.
             for edge in instant_edges - old_edges:
                 instant_distance = instant_goodput[edge]
-                yb = instant_goodput[edge]
                 ya = 0
+                yb = instant_goodput[edge]
                 active_contacts_data[edge] = self.integrate(ya,yb)
                 active_contacts_start[edge] = time - self.time_granularity
 
@@ -81,8 +82,8 @@ class GeographicTrace(object):
                 #this is a previously established contact
                 instant_distance = instant_goodput[edge]
                 previous_distance = old_graph[edge]
-                yb = instant_goodput[edge]
                 ya = old_graph[edge]
+                yb = instant_goodput[edge]
                 active_contacts_data[edge] += self.integrate(ya,yb)
 
             #If there are contact that previously existed and are not found in the current one, consider it terminated.
@@ -99,4 +100,58 @@ class GeographicTrace(object):
             old_edges = instant_edges
             old_graph = instant_goodput
 
+        return terminated_contacts
+
+
+
+    
+class ContactTrace(object):
+    def __init__(self, dataset, propagation, modulation,data_kind):
+        folder_ressources="./ressources/proba_duration_capacity"
+        self.propagation=propagation
+        self.modulation=modulation
+        self.rssi_func = np.vectorize(ModelNearbyWoWMoM.DISTANCE_TO_RSSI)
+        self.bps_func = np.vectorize(ModelNearbyWoWMoM.RSSI_TO_BPS)
+        self.dataset=dataset
+        
+        if data_kind == "human":
+            data_source="stockholm"
+        elif data_kind == "vehicle":
+            data_source="luxembourg"
+        else:
+            print("Data kind '%s' not recognized.")
+            sys.exit(5)
+        precomputed_file_name="%s/%s/%s_%s_%s.json" % (folder_ressources,
+                                           data_source,
+                                           data_source,
+                                           propagation.func_name,
+                                           modulation.func_name)
+        try:
+            precomputed_file_handle=open(precomputed_file_name,"r")
+            self.presampled_contacts=json.load(precomputed_file_handle)
+        except:
+            print("Cannot open the file '%s'" % precomputed_file_name)
+            sys.exit(6)
+        self.sampling_granularity=np.absolute(eval(self.presampled_contacts.keys()[0]))
+        #Simply unpack the elements from the sampled datasets found in the json file.
+        self.contact_time_to_throughput_probability={}
+        for time_key in self.presampled_contacts:
+            self.contact_time_to_throughput_probability[time_key]=zip(*self.presampled_contacts[time_key])
+        self.number_samples=len(self.contact_time_to_throughput_probability)
+        
+    def get_capacity(self):
+        terminated_contacts={}
+        for id1,id2,time_start_raw,time_end_raw in ContactReader(self.dataset):
+            #todo: take time format in consideration. For now just make the difference.
+            time_start=datetime.fromtimestamp(float(time_start_raw))
+            time_end=datetime.fromtimestamp(float(time_end_raw))
+            time_contact=time_end-time_start
+            if time_contact.total_seconds() > 0:
+                index_sampling=int((time_contact.total_seconds() // self.sampling_granularity)*self.sampling_granularity)
+                formated_time_key="%s-%s" % (index_sampling,index_sampling+self.sampling_granularity)
+                throughput_values,probability_value=self.contact_time_to_throughput_probability[formated_time_key]
+                final_edge_key = "contact:%s-%s;time:%s-%s" % (id1,id2, time_start_raw, time_end_raw)
+                terminated_contacts[final_edge_key]=np.random.choice(throughput_values,p=probability_value)
+                
+                
         return terminated_contacts
