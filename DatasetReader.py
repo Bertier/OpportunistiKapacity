12f6d7b895991eff105cf9a4d1cd578b38892c51
Clément Bertier <clement.bertier@lip6.fr>
2019-04-09 13:50:40 +0200

    Correctly parse all files.

--- DatasetReader.py
+++ DatasetReader.py
@@ -15,12 +15,13 @@ int_re=r'[-+]?[0-9]*'
 string_re=r'\S+'
 any_re=r'.+?'
 
-time_re=r'(?P<time>%s)' % float_re
+
 dummy_re= any_re
+
+time_re=r'(?P<time>%s)' % float_re
 id_node_re=r'(?P<id_node>%s)' % string_re
 pos_x_re=r'(?P<pos_x>%s)' % float_re
 pos_y_re=r'(?P<pos_y>%s)' % float_re
-
 mobility_params={
     "time":time_re,
     "dummy": dummy_re,
@@ -28,6 +29,19 @@ mobility_params={
     "y":pos_y_re,
     "id":id_node_re
 }
+
+id1_node_re=r'(?P<id1_node>%s)' % string_re
+id2_node_re=r'(?P<id2_node>%s)' % string_re
+start_re=r'(?P<start>%s)' % float_re
+end_re=r'(?P<end>%s)' % float_re
+contact_params={
+    "dummy": dummy_re,
+    "start":start_re,
+    "end":end_re,
+    "id1":id1_node_re,
+    "id2":id2_node_re
+}
+
 
 class DatasetReader(object):
     """
@@ -44,7 +58,7 @@ class DatasetReader(object):
         #Infer the regular expression from the configuration file.
         raw_file_format=cfg.get('mobility-trace', 'file_parsing')
         column_delimiter=cfg.get('mobility-trace','column_delimiter') if len(cfg.get('mobility-trace','column_delimiter')) else '\s'
-        pre_regex_format="^{" + ("}%s+{" % column_delimiter).join(raw_file_format.split()) + "}"
+        pre_regex_format=("^%s*{" % column_delimiter) + ("}%s+{" % column_delimiter).join(raw_file_format.split()) + "}"
         self.line_regex=re.compile(r'%s'% pre_regex_format.format(**mobility_params))
         #Get the first timestamp of the file.
         self.file_handle=open(dataset,"r")
@@ -95,14 +109,26 @@ class ContactReader(object):
             sys.exit(0)
         self.file_handle=open(dataset,"r")
         self.field_separator=field_separator
-
+        cfg = ConfigParser.ConfigParser()
+        cfg.read(name_configuration_file)
+        raw_file_format=cfg.get('contact-trace', 'file_parsing')
+        column_delimiter=cfg.get('contact-trace','column_delimiter') if len(cfg.get('mobility-trace','column_delimiter')) else '\s'
+        pre_regex_format=("^%s*{" % column_delimiter) + ("}%s+{" % column_delimiter).join(raw_file_format.split()) + "}"
+        self.line_regex=re.compile(r'%s'% pre_regex_format.format(**contact_params))
+        self.line=1
     def __iter__(self):
         return self
     
     def next(self):
         #TODO: Field seperator fix
-        fields=self.file_handle.readline().split()
-        if len(fields):
-            return fields[0:4]
+        line=self.file_handle.readline()
+        if len(line):
+            fields=self.line_regex.search(line)
+            if fields is None:
+                print("Error in dataset file at line %d. Please check format." % self.line_number)
+                sys.exit(10)
+            else:
+                return [fields.group('id1_node'),fields.group('id2_node'),fields.group('start'),fields.group('end')]
         else:
             raise StopIteration
+        self.line+=1
