607ac9caa2f13fbccf8e546076259c5a8490b979
Clément Bertier <clement.bertier@lip6.fr>
2019-04-24 14:59:44 +0200

    Correct the setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 setup(
-  name='opportunistikapacity',         # How you named your package folder (MyLib)
-  packages=['opportunistikapacity'],   # Chose the same as "name"
-  version='0.0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description='A simple library to compute the contact capacity in mobility/contact traces',   # Give a short description about your library
-  author='Clement Bertier',                   # Type in your name
-  author_email='clement.bertier@lip6.fr',      # Type in your E-Mail
-  url='https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  name='opportunistikapacity',
+  packages=['opportunistikapacity'],
+  version='0.0.1',
+  license='MIT',
+  description='A simple library to compute the contact capacity in mobility/contact traces',
+  author='Clement Bertier',
+  author_email='clement.bertier@lip6.fr',
+  url='https://github.com/Bertier/OpportunistiKapacity',
   download_url='https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords=['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  keywords=['CONTACT', 'OPPORTUNISTIC', 'CAPACITY', 'MOBILITY', 'DATA', 'TRACE', 'VANET', 'MANET'],
   install_requires=[            # I get to this in a second
           'numpy',
           'scipy',
@@ -21,7 +21,8 @@ setup(
 
     'License :: OSI Approved :: MIT License',   # Again, pick a license
 
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
   ],
 )
